cd1a14a20f5bd954ee14df45e97bd4fdb089b565
Jamie Burkart <jamie@ohai.us>
2024-10-26 21:55:34 -0400

    Increase maxBuffer size in runPythonScript to handle large outputs
    - Updated run-python-script.js to set maxBuffer to 20MB to prevent stdout maxBuffer exceeded errors

--- utils/run-python-script.js
+++ utils/run-python-script.js
@@ -13,23 +13,31 @@ export async function runPythonScript(
   console.log(`Executing command:\n${command}`);
 
   return new Promise((resolve, reject) => {
-    exec(command, async (error, stdout, stderr) => {
-      if (error) {
-        console.error(
-          `Error executing Python script ${scriptPath}:\n${stderr}`
-        );
-        reject(error);
-        return;
+    exec(
+      command,
+      {
+        maxBuffer: 1024 * 1024 * 20, // Increase buffer to 20MB
+      },
+      async (error, stdout, stderr) => {
+        if (error) {
+          console.error(
+            `Error executing Python script ${scriptPath}:\n${stderr}`
+          );
+          reject(error);
+          return;
+        }
+        // Write stdout to the outputPath
+        try {
+          await fs.writeFile(outputPath, stdout, "utf-8");
+          console.log(`Output written to ${outputPath}`);
+          resolve();
+        } catch (writeError) {
+          console.error(
+            `Error writing output to ${outputPath}:\n${writeError}`
+          );
+          reject(writeError);
+        }
       }
-      // Write stdout to the outputPath
-      try {
-        await fs.writeFile(outputPath, stdout, "utf-8");
-        console.log(`Output written to ${outputPath}`);
-        resolve();
-      } catch (writeError) {
-        console.error(`Error writing output to ${outputPath}:\n${writeError}`);
-        reject(writeError);
-      }
-    });
+    );
   });
 }
