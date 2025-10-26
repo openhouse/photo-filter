e723cef536db1da0cfd0c3619948cd4756b8fda2
Jamie Burkart <jamie@ohai.us>
2025-10-26 19:34:55 -0400

    use ramdb

--- backend/utils/export-images.js
+++ backend/utils/export-images.js
@@ -48,7 +48,7 @@ export async function runOsxphotosExportImages(
 
   const cmd = `"${osxphotosPath}" export "${imagesDir}" \
   --uuid-from-file "${uuidsFile}" \
-  --download-missing --use-photokit \
+  --download-missing --use-photokit --ramdb \
   --filename "${filenameTemplate}" \
   --convert-to-jpeg --jpeg-ext jpg`;
 
